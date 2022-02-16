fd0ed7781c02baebd891e7131f63981b5134e7d9
luwyo <90765843+luwyo@users.noreply.github.com>
2022-02-16 18:02:28 +0300

    Delete app/src/main/java/ru/gb/course1/filmssearch/view_model directory

--- app/src/main/java/ru/gb/course1/filmssearch/view_model/AppState.kt
+++
@@ -1,9 +0,0 @@
-package ru.gb.course1.filmssearch.view_model
-
-import ru.gb.course1.filmssearch.model.Film
-
-sealed class AppState {
-    data class Success(val filmsData: List<Film>) : AppState()
-    data class Error(val error: Throwable) : AppState()
-    object Loading : AppState()
-}
